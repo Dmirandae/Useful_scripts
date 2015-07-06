4831840ac92a537168e859f6a85489eb0f739389
oleon12 <leon.alvarado12@gmail.com>
2015-07-06 12:17:27 -0500

    ToNumber

--- ToNumber.R
+++
@@ -1,9 +0,0 @@
-toNum <- function(x){
- for (i in 1:length(x)){
-   matrix <- as.matrix(x)
-   x[,i] <- as.numeric(matrix[,i])  
- }
-}
- 
-#At the end, all characters in the data frame will be numeric type variables.
-
