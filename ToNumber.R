180037ec9b3aa96c6fb4462476d78c503ae01133
Omar Daniel Leon Alvarado <leon.alvarado12@gmail.com>
2016-10-26 17:00:48 -0500

    Update ToNumber.R

--- ToNumber.R
+++ ToNumber.R
@@ -3,12 +3,17 @@
 #
 # leon.alvarado12@gmail.com
 
-toNum <- function(x){
- for (i in 1:length(x)){
-   matrix <- as.matrix(x)
-   x[,i] <- as.numeric(matrix[,i])  
+toNum <-function(x){
+ out <- matrix(NA, nrow = length(rownames(x)), ncol = length(colnames(x)))
+ for (i in 1:length(colnames(x))){
+   out[,i] <- as.numeric(x[,i])
  }
+ colnames(out) <- colnames(x)
+ rownames(out) <- rownames(x)
+ 
+ return(out)
 }
+
  
 #At the end, all characters in the data frame will be numeric type variables.
 
